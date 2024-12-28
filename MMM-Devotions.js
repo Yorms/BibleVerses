a2ea2b5da08ce2235c1430ed7ef3d70f38f5da2c
Yorms <eliwood232@live.com>
2024-12-27 22:27:24 -0500

    Update MMM-Devotions.js

--- MMM-Devotions.js
+++ MMM-Devotions.js
@@ -1,7 +1,7 @@
 Module.register("MMM-Devotional", {
     defaults: {
-        apiUrl: "https://api.browse.ai/v2/tasks/b70be40d-dad7-4713-81fa-bbd3cd6d9689/run", // Replace {robot_id} with your BrowseAI Robot ID
-        apiKey: "f354afee-6e46-4651-a93d-064aa50785d4:b2d0528f-def0-4fde-8b6e-d4804e1e00bc", // Replace with your BrowseAI API Key
+        apiUrl: "https://api.browse.ai/v2/tasks/{inser_Robot_ID}/run", // Replace {robot_id} with your BrowseAI Robot ID
+        apiKey: "", // Replace with your BrowseAI API Key
         updateInterval: 24 * 60 * 60 * 1000 // Update every 24 hours
     },
 
