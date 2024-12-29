6d33175eb1f414f2b0f3b88481fc0c3b7785de87
Yorms <eliwood232@live.com>
2024-12-29 00:36:22 -0500

    Update MMM-DailyDevotional.js

--- MMM-DailyDevotional.js
+++ MMM-DailyDevotional.js
@@ -2,7 +2,7 @@ Module.register("MMM-DailyDevotional", {
     defaults: {
         apiUrl: "https://api.browse.ai/v2/robots/b70be40d-dad7-4713-81fa-bbd3cd6d9689/monitors/DailyDevotional",
         apiKey: "f354afee-6e46-4651-a93d-064aa50785d4:b2d0528f-def0-4fde-8b6e-d4804e1e00bc",
-        updateInterval: 24 * 60 * 60 * 1000, // Default: fetch data every 24 hours
+        updateInterval: 24 * 60 * 60 * 5000, // Default: fetch data every 24 hours
     },
 
     start: function () {
